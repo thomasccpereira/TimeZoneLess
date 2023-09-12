79d915786c371d093fa6dfa4b19f686574c40c7e
thomasccpereira <thomascalmon.cp@gmail.com>
2023-09-12 11:18:44 +0100

    removed default content view

--- TimeZoneLess/TimeZoneLessApp.swift
+++ TimeZoneLess/TimeZoneLessApp.swift
@@ -9,12 +9,5 @@ import SwiftUI
 
 @main
 struct TimeZoneLessApp: App {
-    let persistenceController = PersistenceController.shared
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
-    }
+   
 }
